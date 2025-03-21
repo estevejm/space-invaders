50387701783b70968703fabdf812c751e5753f3a
Esteve Julià <estevejuliamelis@gmail.com>
2025-03-21 01:32:03 +0100

    check memory bounds

--- src/memory.c
+++ src/memory.c
@@ -3,15 +3,25 @@
 #include <string.h>
 #include "memory.h"
 
+void check_bounds(int address) {
+    if (address < 0 || address >= MEMORY_BYTES) {
+      printf("Error: address %x out of bounds\n", address);
+      exit(1);
+    }
+}
+
+void check_bounds_range(int address, int size) {
+  check_bounds(address);
+  check_bounds(address + size - 1);
+}
+
 void memory_write(Memory *memory, uint8_t bytes[], int address, int size) {
-  if (address + size > MEMORY_BYTES) {
-    printf("Error: address out of bounds\n");
-    exit(1);
-  }
+  check_bounds_range(address, size);
   memcpy(memory->bytes + address, bytes, size);
 }
 
 void memory_peek(Memory *memory, int address, int size) {
+  check_bounds_range(address, size);
   for (int i = address; i < address + size; i++) {
     if (i % 16 == 0) {
       printf("%04x", i);
@@ -27,6 +37,7 @@ void memory_peek(Memory *memory, int address, int size) {
 }
 
 void memory_peek_highlight(Memory *memory, int address, int size, int highlight) {
+  check_bounds_range(address, size);
   int to = address + size;
   for (int i = address; i < address + size; i++) {
     if (i % 16 == 0) {
@@ -51,9 +62,11 @@ void memory_dump(Memory *memory) {
 }
 
 void memory_write_byte(Memory *memory, uint16_t address, uint8_t data) {
+  check_bounds(address);
   memory->bytes[address] = data;
 }
 
 uint8_t memory_read_byte(Memory *memory, uint16_t address) {
+  check_bounds(address);
   return memory->bytes[address];
 }
--- src/memory.h
+++ src/memory.h
@@ -4,8 +4,9 @@
 #define MEMORY_H
 
 // TODO: split in ROM, RAM, VRAM, stack, ...
-//#define MEMORY_BYTES (1 << 14)
-#define MEMORY_BYTES (1 << 13) + 0x400 // space for only ROM + RAM to detect if program tries to write to VRAM space
+//#define MEMORY_BYTES (1 << 14) // this is space invaders available memory
+#define MEMORY_BYTES (1 << 16) // 16 bytes of addressable memory
+//#define MEMORY_BYTES (1 << 13) + 0x400 // space for only ROM + RAM to detect if program tries to write to VRAM space
 
 typedef struct memory {
   uint8_t bytes[MEMORY_BYTES];
